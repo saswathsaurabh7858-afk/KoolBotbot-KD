d1bd550a09f20a3a561cc5f7e3ae392011907663
saswathsaurabh7858-afk <saswathsaurabh7858@gmail.com>
2025-11-06 15:32:50 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-const settings = {
-  packname: 'Knight Bot',
+isconst settings = {
+  packname: 'Mr Elvish',
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '917909084910', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
